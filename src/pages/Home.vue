0d29daa2e834b06a6721290c2cba0cc433718fd6
Francisco <javiermartincuenca@gmail.com>
2021-05-08 11:19:55 +0200

    List tasks component

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,32 +1,7 @@
 <template>
   <div class="home">
     <field-add-task />
-    <v-list flat class="pa-0" v-if="this.tasks.length > 0">
-      <div v-for="task in tasks" :key="task.id">
-        <v-list-item
-          @click="toggleTask(task.id)"
-          :class="{ 'blue lighten-5': task.done }"
-        >
-          <template v-slot:default>
-            <v-list-item-action>
-              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title
-                :class="{ 'text-decoration-line-through': task.done }"
-                >{{ task.title }}</v-list-item-title
-              >
-            </v-list-item-content>
-            <v-list-item-action>
-              <v-btn icon @click.stop="deleteTask(task.id)">
-                <v-icon color="primary lighten-1">mdi-delete</v-icon>
-              </v-btn>
-            </v-list-item-action>
-          </template>
-        </v-list-item>
-        <v-divider />
-      </div>
-    </v-list>
+    <list-tasks v-if="this.tasks.length > 0" :tasks="this.tasks" />
     <div v-else class="no-tasks">
       <v-icon size="100" color="primary"> mdi-check </v-icon>
       <div class="text-h5 primary--text">No tasks</div>
@@ -37,9 +12,10 @@
 <script>
 import { mapActions, mapGetters } from "vuex";
 import FieldAddTask from "@/components/Todo/FieldAddTask.vue";
+import ListTasks from "../components/Todo/ListTasks.vue";
 
 export default {
-  components: { FieldAddTask },
+  components: { FieldAddTask, ListTasks },
   computed: {
     ...mapGetters(["tasks"]),
   },
