99a0afad75f50f7b8b3623476aea3864314ea711
Francisco <javiermartincuenca@gmail.com>
2021-05-09 13:10:11 +0200

    Reorder vissually

--- src/components/Todo/ListTasks.vue
+++ src/components/Todo/ListTasks.vue
@@ -1,14 +1,17 @@
 <template>
   <v-list flat class="pa-0">
-    <task v-for="task in tasks" :key="task.id" :task="task" />
+    <draggable :list="tasks" handle=".handle">
+      <task v-for="task in tasks" :key="task.id" :task="task" />
+    </draggable>
   </v-list>
 </template>
 
 <script>
+import draggable from "vuedraggable";
 import Task from "@/components/Todo/Task.vue";
 
 export default {
-  components: { Task },
+  components: { Task, draggable },
   props: {
     tasks: {
       type: Array,
--- src/components/Todo/Task.vue
+++ src/components/Todo/Task.vue
@@ -3,6 +3,8 @@
     <v-list-item
       @click="toggleTask(task.id)"
       :class="{ 'blue lighten-5': task.done }"
+      class="white"
+      :ripple="false"
     >
       <template v-slot:default>
         <v-list-item-action>
@@ -24,7 +26,7 @@
           <task-menu :task="task" />
         </v-list-item-action>
         <v-list-item-action v-if="isSortingTasks">
-          <v-btn color="primary" icon>
+          <v-btn class="handle" color="primary" icon>
             <v-icon>mdi-drag-horizontal-variant</v-icon>
           </v-btn>
         </v-list-item-action>
@@ -67,5 +69,12 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+.sortable-ghost {
+  opacity: 0;
+}
+
+.sortable-drag {
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+}
 </style>
