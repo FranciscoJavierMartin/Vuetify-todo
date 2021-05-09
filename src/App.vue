c8c327eacf6b20148dac30a8e3848c3f583722c9
Francisco <javiermartincuenca@gmail.com>
2021-05-09 11:41:31 +0200

    Place avatar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <v-app id="inspire">
     <v-navigation-drawer v-model="drawer" app>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title"> Vuetify Todo </v-list-item-title>
-          <v-list-item-subtitle> Best Todo Ever </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
+      <v-img
+        class="pa-2 pt-7"
+        max-height="170"
+        src="https://picsum.photos/id/11/500/300"
+        gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+      >
+        <v-avatar size="70" class="mb-2">
+          <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John Doe" />
+        </v-avatar>
+        <div class="white--text subtitle-1 font-weight-bold">John Doe</div>
+        <div class="white--text subtitle-2">john@doe.com</div>
+      </v-img>
 
       <v-list dense nav>
         <v-list-item v-for="item in items" :key="item.title" link :to="item.to">
